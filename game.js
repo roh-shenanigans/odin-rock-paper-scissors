0365c40160b4ccf5b7d43cb3df672281f02c152f
roh-shenanigans <142331034+roh-shenanigans@users.noreply.github.com>
2026-01-18 10:03:17 +0530

    Add function for computer's choice

---
+++ game.js
@@ -0,0 +1,14 @@
+humanScore = 0;
+computerScore = 0;
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    if (choice == 0) {
+        return "rock";
+    } else if (choice == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
