3527c723f0286fe5ff66ec19b7cd0279c13baa82
roh-shenanigans <142331034+roh-shenanigans@users.noreply.github.com>
2026-01-18 11:24:05 +0530

    Add function to play a round

--- game.js
+++ game.js
@@ -21,3 +21,47 @@ function getHumanChoice() {
 
 // let msg = getHumanChoice();
 // console.log(msg);
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == computerChoice) {
+        console.log("Draw!");
+        return;
+    }
+
+    if (computerChoice == "rock") {
+        if (humanChoice == "paper") {
+            console.log("Human wins!");
+            humanScore += 1;
+        } else if (humanChoice == "scissors") {
+            console.log("Computer wins");
+            computerScore += 1;
+        }
+
+    } else if (computerChoice == "paper") {
+        if (humanChoice == "rock") {
+            console.log("Computer wins!");
+            computerScore += 1;
+        } else if (humanChoice == "scissors") {
+            console.log("Human wins");
+            humanScore += 1;
+        }
+    }
+
+    else if (computerChoice == "scissors") {
+        if (humanChoice == "rock") {
+            console.log("Human wins");
+            humanScore += 1;
+        } else if (humanChoice == "paper") {
+            console.log("Computer wins!");
+            computerScore += 1;
+        }
+    }
+}
+
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+// playRound(humanSelection, computerSelection);
